2b23738b1e87b2b5f47d41484f52813578821b67
ivanng2014 <61515871+ivanng2014@users.noreply.github.com>
2020-03-13 00:12:08 +0800

    Updates

---
+++ precache-manifest.642e661d9026334f89f2ef6a8f6b7439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb92572670539d1bf266523210bab166",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "b788960c0e1d20c0773e",
+    "url": "/react-todo/static/css/main.d60a3d9d.chunk.css"
+  },
+  {
+    "revision": "b30df83f59d536c3bb38",
+    "url": "/react-todo/static/js/2.7c1fb779.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-todo/static/js/2.7c1fb779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b788960c0e1d20c0773e",
+    "url": "/react-todo/static/js/main.cb15c3e0.chunk.js"
+  },
+  {
+    "revision": "168e090078e667cda6c3",
+    "url": "/react-todo/static/js/runtime-main.d35fb694.js"
+  }
+]);
